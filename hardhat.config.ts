c759cb2f724075fb6b01ae030f24df860e40ea33
zaid <zaid.tab123@gmail.com>
2024-03-12 01:20:47 +0530

    initialise project

---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
+const SHARDEUM_RPC = process.env.SHARDEUM_RPC;
+const privateKey = process.env.PRIVATE_KEY;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    shardeum: {
+      url: SHARDEUM_RPC,
+      accounts: [privateKey!],
+      chainId: 8080,
+    },
+  },
+};
+
+export default config;
