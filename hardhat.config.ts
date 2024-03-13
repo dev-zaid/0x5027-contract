390d5b3b350b776002b6352ca7d38213df6182c9
zaid <zaid.tab123@gmail.com>
2024-03-13 16:33:56 +0530

    configure shardeum

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,22 +6,33 @@ const SHARDEUM_RPC = process.env.SHARDEUM_RPC;
 const privateKey = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "shardeum",
+  networks: {
+    hardhat: {},
+    
+    shardeum: {
+      url: SHARDEUM_RPC,
+      chainId: 8082,
+      accounts: [privateKey!],
+    },
+  },
   solidity: {
+    //configure solidity version for compilation
     version: "0.8.24",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
       },
     },
   },
-  networks: {
-    shardeum: {
-      url: SHARDEUM_RPC,
-      accounts: [privateKey!],
-      chainId: 8080,
-    },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 20000,
   },
 };
 
